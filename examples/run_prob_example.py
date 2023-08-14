1246b322cd22d4894bb415d7c881a5799112218a
jdavidrcamacho <jdavidrcamacho@gmail.com>
2023-08-14 11:32:17 +0100

    python example with subprocess!

--- examples/run_prob_example.py
+++ examples/run_prob_example.py
@@ -1,8 +1,19 @@
-import pddlpy
+# import pddlpy
+# domprob = pddlpy.DomainProblem('prob_domain.pddl', 'prob_problem.pddl')
+# print('initial state:', domprob.initialstate())
+# print('operators:', list(domprob.operators()))
 
 
-domprob = pddlpy.DomainProblem('prob_domain.pddl', 'prob_problem.pddl')
+import subprocess
+
+domain_file = '/home/camacho/GitHub/python_pddl/examples/prob_domain.pddl'
+problem_file = '/home/camacho/GitHub/python_pddl/examples/prob_problem.pddl'
+fast_downward_executable = "/home/camacho/GitHub/downward/fast-downward.py"
+
+command = [fast_downward_executable, domain_file, problem_file, "--search", "lazy_greedy([ff()], preferred=[ff()])"]
+result = subprocess.run(command, check=True, text=True)
 
 
-print('initial state:', domprob.initialstate())
-print('operators:', list(domprob.operators()))
+#cmd = [fast_downward_executable, "--search", "lama-first", domain_file, problem_file]
+#retcode = subprocess.call(cmd, shell=True, check=True, capture_output=True, text=True)
+#print(retcode)
