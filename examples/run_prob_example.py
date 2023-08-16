983bb0772a8cefac0695ac53408de519926b083b
jdavidrcamacho <jdavidrcamacho@gmail.com>
2023-08-16 09:37:14 +0100

    pre-commit hook

--- examples/run_prob_example.py
+++ examples/run_prob_example.py
@@ -1,13 +1,21 @@
 import subprocess
+import pytest
 
 domain_file = '/home/camacho/GitHub/python_pddl/examples/prob_domain.pddl'
 problem_file = '/home/camacho/GitHub/python_pddl/examples/prob_problem.pddl'
 fast_downward_executable = "/home/camacho/GitHub/downward/fast-downward.py"
 
 command = [fast_downward_executable, domain_file, problem_file, "--search", "lazy_greedy([ff()], preferred=[ff()])"]
-result = subprocess.run(command, check=True, text=True)
+# result = subprocess.run(command, check=True, text=True)
 
 
-#cmd = [fast_downward_executable, domain_file, problem_file, "--search", "lama-first"]
-#retcode = subprocess.call(cmd,  check=True, capture_output=True, text=True)
-#print(retcode)
+# cmd = [fast_downward_executable, domain_file, problem_file, "--search", "lama-first"]
+# retcode = subprocess.call(cmd,  check=True, capture_output=True, text=True)
+# print(retcode)
+
+def sum_ab(a: float, b: float) -> float:
+    return a+b
+
+
+if __name__ == '__main__':
+  subprocess.run(command, check=True, text=True)
---
+++ examples/test_run_prob_example.py
@@ -0,0 +1,9 @@
+import run_prob_example
+
+def test_sum_ab():
+    output = run_prob_example.sum_ab(1, 3)
+    assert isinstance(output, float), 'ERROR: Not float'
+
+
+if __name__ == '__main__':
+  test_sum_ab()
