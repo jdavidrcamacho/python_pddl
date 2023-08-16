2c8a274c47705363adf0eb60463d8ae635bbd8a8
jdavidrcamacho <jdavidrcamacho@gmail.com>
2023-08-16 10:58:24 +0100

    robot example

--- files/run_example_robot.py
+++ files/run_example_robot.py
@@ -1,7 +1,7 @@
 import subprocess
 
-domain_file = '/home/camacho/GitHub/python_pddl/examples/example_robot_domain.pddl'
-problem_file = '/home/camacho/GitHub/python_pddl/examples/example_robot_problem.pddl'
+domain_file = '/home/camacho/GitHub/python_pddl/examples/files_robot_domain.pddl'
+problem_file = '/home/camacho/GitHub/python_pddl/examples/files_robot_problem.pddl'
 fast_downward_executable = "/home/camacho/GitHub/downward/fast-downward.py"
 
 command = [fast_downward_executable, domain_file, problem_file, "--search", "lazy_greedy([ff()], preferred=[ff()])"]
